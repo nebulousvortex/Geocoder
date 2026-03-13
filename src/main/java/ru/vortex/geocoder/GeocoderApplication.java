f4c824698feaddddd818cc8017da7adc5d617615
nebulousvortex <vortexofnebula@mail.ru>
2026-03-13 22:31:45 +0300

    feat: add swagger UI

--- src/main/java/ru/vortex/geocoder/GeocoderApplication.java
+++ src/main/java/ru/vortex/geocoder/GeocoderApplication.java
@@ -1,11 +1,20 @@
 package ru.vortex.geocoder;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Geocoder API",
+				version = "1.0",
+				description = "REST API для геокодирования локаций и построения маршрутов"
+		)
+)
 public class GeocoderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GeocoderApplication.class, args);
