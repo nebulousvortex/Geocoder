8760ed84428e3226bf6313ba361ac81bce9dc538
nebulousvortex <vortexofnebula@mail.ru>
2026-03-21 20:15:47 +0300

    fix: fix migration v2 file

--- src/main/resources/db/migration/V2__add_default_statuses.sql
+++ src/main/resources/db/migration/V2__add_default_statuses.sql
@@ -1,29 +1,3 @@
-CREATE TABLE IF NOT EXISTS statuses (
-    id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(50) UNIQUE NOT NULL,
-    color VARCHAR(7) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS users (
-    id BIGSERIAL PRIMARY KEY,
-    username VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    role VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS locations (
-    id BIGSERIAL PRIMARY KEY,
-    address VARCHAR(500) UNIQUE NOT NULL,
-    latitude DOUBLE PRECISION,
-    longitude DOUBLE PRECISION,
-    aliases VARCHAR(2000),
-    normalized_address VARCHAR(2000),
-    country VARCHAR(100),
-    city VARCHAR(100),
-    status_id BIGINT NOT NULL REFERENCES statuses(id),
-    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
-
 INSERT INTO statuses (name, color) VALUES ('В процессе', '#FFA500')
 ON CONFLICT (name) DO NOTHING;
 
